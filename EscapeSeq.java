12fea42f6b5d889cb4f48230fcca148c1ccb4d9e
capybaraEnjoyer <108890297+capybaraEnjoyer@users.noreply.github.com>
2024-10-19 20:50:48 +0200

    Create EscapeSeq.java

---
+++ EscapeSeq.java
@@ -0,0 +1,25 @@
+public class EscapeSeq {
+
+    //TECLES ESPECIALS
+    public final static int RIGHT = 67; //C
+    public final static int LEFT = 68; //D
+    public final static int START = 72; //H
+    public final static int END = 70; //F
+    public final static int INSERT = 50; //2
+    public final static int SUPR = 51; //3
+    public final static int DELETE = 127;
+    public final static int ENTER = 13; //enter
+    public final static int UP = 65;//A
+    public final static int DOWN = 66;//B
+    public final static int CTLRC = 3;
+
+    //CARÀCTERS IMPORTANTS
+    public final static int ESC = 27; //^[
+    public final static int BRACKET = 91; //[
+    public final static int TILDE = 126; //~
+    public final static int CR = 13; //cr
+
+    //
+    public final static int EMPTY = -1;
+    
+}
