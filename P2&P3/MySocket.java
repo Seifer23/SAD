b9c853959b169df649449960f215f8c6c672792e
capybaraEnjoyer <108890297+capybaraEnjoyer@users.noreply.github.com>
2024-11-11 18:42:43 +0100

    Update MySocket.java

--- P2&P3/MySocket.java
+++ P2&P3/MySocket.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -8,11 +9,17 @@ public class MySocket{
     private BufferedReader bufferedReader;
     private PrintWriter printWriter;
 
-    public void MySocket(InetAddress address, int port) {
+    public MySocket(InetAddress address, int port) {
         try {
-            
-        } catch (Exception e) {
-           
+            this.socket = new Socket(address, port);
+        } catch (IOException e) {
+            System.out.println("Error creating socket:" + e.getMessage());
+        } catch (SecurityException e) {
+            System.out.println("The checkConnect method doesn't allow the operation:" + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Port parameter error: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("Address is null: " + e.getMessage());
         }
     }
 }
