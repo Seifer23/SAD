c4e6b085c169067646eb2a627da2c124d17be49b
capybaraEnjoyer <108890297+capybaraEnjoyer@users.noreply.github.com>
2024-11-11 16:55:25 +0100

    Create MySocket.java

---
+++ P2&P3/MySocket.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class MySocket{
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+
+    public void MySocket(InetAddress address, int port) {
+        try {
+            
+        } catch (Exception e) {
+           
+        }
+    }
+}
