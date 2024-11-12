f1537295401e8ed754a5e9b0c7ea45eececfdb57
capybaraEnjoyer <jesus.azcam@gmail.com>
2024-11-12 18:36:20 +0100

    Server Constructor added

--- src/ClientTextualXat/MyServerSocket.java
+++ src/ClientTextualXat/MyServerSocket.java
@@ -1,8 +1,24 @@
 package ClientTextualXat;
 import java.io.*;
 import java.net.Socket;
+import java.net.ServerSocket;
 
 public class MyServerSocket {
+    private ServerSocket serverSocket;
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+
+    public MyServerSocket(int port){
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Error creating socket:" + e.getMessage());
+        } catch (SecurityException e) {
+            System.out.println("The checkConnect method doesn't allow the operation:" + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Port parameter error: " + e.getMessage());
+        }
+    }
 
 
 }
